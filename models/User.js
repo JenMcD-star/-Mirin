1c39b93266f3f32f057ce688d11c1ddcfd3ca30a
JenMcD-star <jenniferannvancura@gmail.com>
2023-02-23 12:01:17 -0600

    Add additional security packages

--- models/User.js
+++ models/User.js
@@ -6,13 +6,13 @@ const jwt = require('jsonwebtoken')
 
 const UserSchema = new Schema({  name: {
     type: String,
-    required: [true, 'Please provide name'],
+    required: [true, ' Please provide name'],
     maxlength: 50,
     minlength: 3,
   },
   email: {
     type: String,
-    required: [true, 'Please provide email'],
+    required: [true, ' Please provide email'],
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       'Please provide a valid email',
@@ -21,7 +21,7 @@ const UserSchema = new Schema({  name: {
   },
   password: {
     type: String,
-    required: [true, 'Please provide password'],
+    required: [true,'Please provide password'],
     minlength: 8,
   },
 })
