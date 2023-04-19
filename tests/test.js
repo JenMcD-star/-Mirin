a7300ef8450826af1373db314a3e135dfe7793c1
JenMcD-star <jenniferannvancura@gmail.com>
2023-04-19 14:54:42 -0500

    testing attempts

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,8 @@
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const { app, server } = require("../app");
-
+var expect = require('chai').expect;
+var request = require('supertest');
 chai.use(chaiHttp);
 chai.should();
 
@@ -46,15 +47,53 @@ describe("Activities", () => {
                 });
         });
     })
+    })
+
+    //things I've tried 
+   /*
+    1. this returns an 401 which makes sense.
 
     it("should create an activity entry with valid input", (done) => {
         chai
             .request(app)
-            .post("/api/v1/people")
+            .post("/api/v1/activities")
             .send({ activityName: "Lift Test", liftType: 'Core', weight: 1, reps: 1 })
             .end((err, res) => {
                 res.should.have.status(201);
                 done();
             });
     })
-})
+-----------------------------------------------------------
+2. the tests won't run with this- I think I'm using the before wrong?
+
+    const userCredentials = {
+    email: 'user@user.com', 
+    password: 'user!!!!'
+}
+  var authenticatedUser = request.agent(app);before(function(done){
+    authenticatedUser
+      .post('/api/v1/auth/login')
+      .send(userCredentials)
+      .end(function(err, response){
+        expect(response.statusCode).to.equal(200);
+        done();
+      });
+  });
+------------------------------------------------------------------------
+3. this returns an error of undefined agent
+
+var agent = chai.request.agent(app)
+agent
+  .post('api/v1/login')
+  .send({ username: 'user@user.com', password: 'user!!!!' })
+  .then(function (res) {
+    expect(res).to.have.cookie('sessionid');
+    return agent
+    .post('/api/v1/activities')
+    .sent({activityName: 'Lift Test', liftType: 'Core'}, weight: 1, reps: 1})
+      .then(function (res) {
+         expect(res).to.have.status(201);
+      });
+  });
+
+*/
